7db83800c025a0c445f17ecc47a65bbc8d03e427
Jatin <jatinjangra514@gmail.com>
2026-02-27 16:18:26 +0530

    Add detailed output for selection sort process

--- Array/selectionsort.cpp
+++ Array/selectionsort.cpp
@@ -1,4 +1,4 @@
-
+//selection sort 
 #include <iostream>
 using namespace std;
 int main()
@@ -16,15 +16,27 @@ int main()
         cout<<arr[i]<<" ";
     }
     for(i=0;i<n-1;i++){
+        cout<<"\nPass "<<i+1<<" : "<<endl;
         int min=i;
         for(int j=i+1;j<n;j++){
+            cout<<"min = "<<arr[min]<<" , loc = "<<j<<" / compare "<<arr[min]<<" and "<<arr[j];
             if(arr[min]>arr[j]){
+                cout<<" (Swap)";
                 min=j;
             }
+            else{
+                cout<<" (No Swap)";
+            }
+            cout<<endl;
         }
         int temp= arr[i];
         arr[i]=arr[min];
         arr[min]=temp;
+        
+        cout<<"Array after pass "<<i+1<<" : ";
+        for(int k=0;k<n;k++){
+        cout<<arr[k]<<" ";
+    }
     }
     cout<<"\nArray after sort : ";
     for(i=0;i<n;i++){
